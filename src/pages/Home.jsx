c1ecc4877ec61d1a4e81d9e341ee0ce942e94619
Mahmoud Elhelou <mahelhelou@gmail.com>
2023-08-23 01:12:53 +0300

    Fixed compilation issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,27 @@
 import { Link } from 'react-router-dom'
 import { FaQuestion } from 'react-icons/fa'
 
-import FeedbackList from './components/FeedbackList'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
+import FeedbackList from '../components/FeedbackList'
+import FeedbackStats from '../components/FeedbackStats'
+import FeedbackForm from '../components/FeedbackForm'
 
 // import FeedbackData from './data/FeedbackData'
 
 const Home = () => {
-  // const [feedback, setFeedback] = useState(FeedbackData)
-  return (
-    <>
-      <FeedbackForm />
-      <FeedbackStats />
-      <FeedbackList />
+	// const [feedback, setFeedback] = useState(FeedbackData)
+	return (
+		<>
+			<FeedbackForm />
+			<FeedbackStats />
+			<FeedbackList />
 
-      <div className='about-link'>
-        <Link to='/about'>
-          <FaQuestion size={30} />
-        </Link>
-      </div>
-    </>
-  )
+			<div className='about-link'>
+				<Link to='/about'>
+					<FaQuestion size={30} />
+				</Link>
+			</div>
+		</>
+	)
 }
 
 export default Home
